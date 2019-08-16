873c70f21b7cc2d3ad55fe835024b4128195560c
Alex Tompkins <alextompkins3@gmail.com>
2019-08-16 23:00:03 +1200

    Initial files

---
+++ README.md
@@ -0,0 +1,12 @@
+# COSC401: Machine Learning | Assignment 1 #
+**Author:** Alex Tompkins
+
+
+## Install Instructions
+### Production ###
+To do a production install, run `pip install .` or `pip install -r requirements.txt`.
+
+### Development ###
+To install the package for development, run `pip install -e .[dev]`. This installs all the regular required packages and those listed in the dev section of extras_require.
+
+After adding any dependencies to `setup.py`, regenerate the `requirements.txt` file with `pip-compile`.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    'jupyter',
+    'scikit-learn'
+]
+
+dev_requirements = [
+    'pip-tools',
+]
+
+setup(
+    name='machine_learning_ass1',
+    version='0.0.0',
+    description='Assignment #1 for COSC401-19S2: Machine Learning.',
+    author='Alex Tompkins',
+    author_email='ato47@uclive.ac.nz',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require={
+        'dev': dev_requirements
+    }
+)
