3ce8704097e5eb79549542fa863d666a660c8f56
Alex Tompkins <ato47@uclive.ac.nz>
2019-08-20 16:25:04 +1200

    Fix training/test split and output via pandas DataFrame

--- playground.py
+++ playground.py
@@ -1,11 +1,30 @@
+import pandas
 from sklearn import datasets, metrics
 from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, train_test_split
+from sklearn.utils import Bunch
 from matplotlib import pyplot
 
 
-def evaluate_decision_tree(dataset, max_depth, k):
-    X, y = dataset.data, dataset.target
+def split_data(dataset):
+    X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target)
+    training_data = Bunch()
+    training_data.data = X_train
+    training_data.target = y_train
+    training_data.feature_names = dataset.feature_names
+    training_data.target_names = dataset.target_names
+
+    testing_data = Bunch()
+    testing_data.data = X_test
+    testing_data.target = y_test
+    testing_data.feature_names = dataset.feature_names
+    testing_data.target_names = dataset.target_names
+
+    return training_data, testing_data
+
+
+def evaluate_depth(training_dataset, max_depth, k):
+    X, y = training_dataset.data, training_dataset.target
     num_nodes = []
     accuracies = []
 
@@ -24,23 +43,46 @@ def evaluate_decision_tree(dataset, max_depth, k):
         accuracy = metrics.accuracy_score(y_test, y_predictions)
         accuracies.append(accuracy)
 
-        plot_tree(decision_tree, feature_names=dataset.feature_names, class_names=dataset.target_names)
+        plot_tree(decision_tree, feature_names=training_dataset.feature_names, class_names=training_dataset.target_names)
 
     avg_num_nodes = sum(num_nodes) / k
     avg_accuracy = sum(accuracies) / k
     return avg_accuracy, avg_num_nodes
 
 
+def print_results(results):
+    max_depth_label = 'Max Depth'
+    accuracy_label = 'Accuracy'
+    num_nodes_label = '# Nodes'
+
+    output = {
+        max_depth_label: [],
+        accuracy_label: [],
+        num_nodes_label: []
+    }
+
+    for max_depth in results.keys():
+        output[max_depth_label].append(max_depth)
+        output[accuracy_label].append(results[max_depth][0])
+        output[num_nodes_label].append(results[max_depth][1])
+
+    print(pandas.DataFrame(output).to_string(index=False, float_format='{:.3f}'.format))
+
+
+# def find_best_max_depth(results):
+#     for
+
+
 def main():
     iris = datasets.load_iris()
+    training_partition, testing_partition = split_data(iris)
 
-    for max_depth in range(1, 50):
-        avg_accuracy, avg_num_nodes = evaluate_decision_tree(iris, max_depth=max_depth, k=7)
+    results = dict()
+    for max_depth in range(1, 20 + 1):
+        avg_accuracy, avg_num_nodes = evaluate_depth(training_partition, max_depth=max_depth, k=7)
+        results[max_depth] = (avg_accuracy, avg_num_nodes)
 
-        print(f'Max depth: {max_depth}')
-        print(f'Avg num nodes: {avg_num_nodes:.1f}')
-        print(f"Avg accuracy: {avg_accuracy:.3f}")
-
+    print_results(results)
     # pyplot.show()
 
 
