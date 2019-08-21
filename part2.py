5e6b96632306ae66c2f142ee10f23e3c084c97b5
Alex Tompkins <alextompkins3@gmail.com>
2019-08-22 02:25:04 +1200

    Part2: fit decision trees for each training_size/max_depth combination, print accuracy results in pivoted table
    Took 46 minutes

--- part2.py
+++ part2.py
@@ -2,6 +2,9 @@ import random
 import numpy
 from matplotlib import pyplot
 from pandas import DataFrame
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 
 SEED = 1337
@@ -33,8 +36,31 @@ def visualise_data(data, target):
 
 
 def main():
-    data, target = generate_random_data(20, 20, 1000)
+    data, target = generate_random_data(20, 20, 2000)
     visualise_data(data, target)
+
+    X_train, X_test, y_train, y_test = train_test_split(data, target, train_size=0.6, random_state=SEED)
+    results = []
+
+    for training_size in (5, 10, 50, 100, 500, 1000):
+        X_train_resized = X_train[:training_size]
+        y_train_resized = y_train[:training_size]
+
+        for max_depth in range(1, 20 + 1):
+            decision_tree = DecisionTreeClassifier(max_depth=max_depth, random_state=SEED)\
+                .fit(X_train_resized, y_train_resized)
+            y_predictions = decision_tree.predict(X_test)
+            accuracy = accuracy_score(y_test, y_predictions)
+            results.append({
+                'training_size': training_size,
+                'max_depth': max_depth,
+                'accuracy': accuracy
+            })
+
+    data_frame = DataFrame(results)
+    pivoted = data_frame.pivot_table(index='max_depth', columns='training_size')
+    # flattened = DataFrame(pivoted.to_records())
+    print(pivoted.to_string(index=True, float_format='{:.4f}'.format))
 
 
 if __name__ == '__main__':
