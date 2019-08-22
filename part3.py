5835d01ce69d0c78b0fe9f8691f900ef3d371e5a
Alex Tompkins <alextompkins3@gmail.com>
2019-08-23 01:59:00 +1200

    Test accuracy of the learned model
    Took 52 minutes

--- part3.py
+++ part3.py
@@ -1,6 +1,6 @@
 import numpy
 import pandas
-from sklearn import linear_model
+from sklearn import linear_model, metrics
 
 
 FILENAME = 'assignment1-2019-data.csv'
@@ -11,47 +11,61 @@ def read_data_from_csv():
     return dataframe
 
 
+def create_piecewise_function(c, intercept):
+    """
+    Creates the piecewise function identified by the regression model. This function will have different weights for
+    each input variable based on the value of the categorical variable.
+    :param c: The learned coefficients of each input variable
+    :param intercept: The y-intercept of the learned model
+    :return: The piecewise function learned by the regression model
+    """
+    weights = numpy.array([
+        [c['X1'], c['X2'], c['X3'], intercept],
+        [c['X1'] + c['X1B'], c['X2'] + c['X2B'], c['X3'] + c['X3B'], intercept + c['DummyB']],
+        [c['X1'] + c['X1C'], c['X2'] + c['X2C'], c['X3'] + c['X3C'], intercept + c['DummyC']]
+    ])
+
+    def predict(row):
+        weight_map = {
+            'A': weights[0],
+            'B': weights[1],
+            'C': weights[2]
+        }
+        coeffs = weight_map[row['X4']]
+        return coeffs[0] * row['X1'] + coeffs[1] * row['X2'] + coeffs[2] * row['X3'] + coeffs[3]
+
+    print(weights)
+    return predict
+
+
 def main():
     dataframe = read_data_from_csv()
+
+    # Add dummy variables and interactions terms based on the 3 levels of the categorical variable
+    # These will also be learned by the model to provide greater accuracy
     dataframe['DummyB'] = dataframe['X4'].apply(lambda category: 1 if category == 'B' else 0)
     dataframe['DummyC'] = dataframe['X4'].apply(lambda category: 1 if category == 'C' else 0)
-
     for col in ('X1', 'X2', 'X3'):
         dataframe[f'{col}B'] = dataframe[col] * dataframe['DummyB']
         dataframe[f'{col}C'] = dataframe[col] * dataframe['DummyC']
 
+    # Carry out linear regression to find coefficients and intercept
     x = dataframe[[col for col in dataframe if col not in ('X4', 'Y')]]
     y = dataframe['Y']
-
     regression = linear_model.LinearRegression().fit(x, y)
-    print(regression)
-    print(x.columns.values)
-    print(regression.intercept_)
-    print(regression.coef_)
+    coeffs = {x.columns.values[i]: int(round(coeff)) for i, coeff in enumerate(regression.coef_)}
+    intercept = int(round(regression.intercept_))
 
-    w = {x.columns.values[i]: coeff for i, coeff in enumerate(regression.coef_)}
+    print(coeffs)
+    print(f'Intercept: {intercept}')
 
-    weights = numpy.array([
-        [w['X1'], w['X2'], w['X3'], regression.intercept_],
-        [w['X1'] + w['X1B'], w['X2'] + w['X2B'], w['X3'] + w['X3B'], regression.intercept_ + w['DummyB']],
-        [w['X1'] + w['X1C'], w['X2'] + w['X2C'], w['X3'] + w['X3C'], regression.intercept_ + w['DummyC']]
-    ])
-
-    def y(x):
-        coeffs = [0, 0, 0, 0]
-        if x[3] == 'A':
-            coeffs = weights[0]
-        elif x[3] == 'B':
-            coeffs = weights[1]
-        elif x[3] == 'C':
-            coeffs = weights[2]
-        return coeffs[0] * x[0] + coeffs[1] * x[1] + coeffs[2] * x[2] + coeffs[3] * regression.intercept_
-
-    print(weights)
-
-    # b1 intercept for A (base case)
-    # b2 is difference in intercept between B and A
-    # b3 is difference in intercept between C and A
+    # Test accuracy of our learned model
+    predict = create_piecewise_function(coeffs, intercept)
+    predictions = dataframe[['X1', 'X2', 'X3', 'X4']].apply(predict, axis=1)
+    mean_sq_error = metrics.mean_squared_error(y, predictions)
+    r_squared_score = metrics.r2_score(y, predictions)
+    print(f'Mean Squared Error: {mean_sq_error}\n'
+          f'R^2 score: {r_squared_score}')
 
 
 if __name__ == '__main__':
